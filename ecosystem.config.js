6b55ac30897e38255da5d27642745479d1138ada
Nemanja <kuzmaneca@gmail.com>
2020-07-17 18:07:51 +0200

    try removing instances

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,9 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: './dist/server.js',
-      instances: 1,
-      exec_mode: 'cluster',
+      script: './dist/app.js',
       env: {
         NODE_ENV: 'development',
       },
