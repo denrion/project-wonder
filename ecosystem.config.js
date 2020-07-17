ebb6bdac26114ae5625c44a4a1fd4ef1ced1fc02
Nemanja <kuzmaneca@gmail.com>
2020-07-17 18:05:52 +0200

    try only 1 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'app',
       script: './dist/server.js',
-      instances: 'max',
+      instances: 1,
       exec_mode: 'cluster',
       env: {
         NODE_ENV: 'development',
